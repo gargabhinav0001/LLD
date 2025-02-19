e0a7e099458904cf34ab9a8c1db83e6f405d6cd4
Abhinav <gargabhinav0001@gmail.com>
2025-02-19 12:56:53 +0530

    fix: accordion

--- low-level-design/app/accordion/AccordionItem.js
+++ low-level-design/app/accordion/AccordionItem.js
@@ -4,15 +4,15 @@ const AccordionItem = ({ title, body, isOpen, setOpenId }) => {
   const arrow = isOpen ? "↑" : "↓";
 
   return (
-    <div
-      onClick={() => {
-        if (isOpen) {
-          setOpenId(null);
-        } else setOpenId(title);
-      }}
-      className="border-2 border-purple-400"
-    >
-      <div className="bg-slate-300 cursor-pointer font-bold p-1 flex justify-between">
+    <div className="border-2 border-purple-400">
+      <div
+        onClick={() => {
+          if (isOpen) {
+            setOpenId(null);
+          } else setOpenId(title);
+        }}
+        className="bg-slate-300 cursor-pointer font-bold p-1 flex justify-between"
+      >
         <p>{title}</p> <span>{arrow}</span>
       </div>
       {isOpen && <div className="bg-slate-400 p-2">{body}</div>}
